869c91c04e8030b65632f9331f1f7ae35c8673da
Bikramdeep Singh <bikramdeep.singh@suse.com>
2019-09-10 10:57:35 -0700

    adding SLE buildpacks pipeline

--- pipelines/release-images/tasks/build.sh
+++ pipelines/release-images/tasks/build.sh
@@ -8,17 +8,16 @@ service docker status
 trap 'service docker stop' EXIT
 sleep 10
 
-echo "${DOCKER_TEAM_PASSWORD_RW}" | docker login --username "${DOCKER_TEAM_USERNAME}" --password-stdin
+# Login to the Docker registry.
+echo "${DOCKER_TEAM_PASSWORD_RW}" | docker login "${DOCKER_REGISTRY}" --username "${DOCKER_TEAM_USERNAME}" --password-stdin
 
-pushd s3.fissile-linux
-tar xfv fissile-*.tgz
-popd
+# Extract the fissile binary.
+tar xvf s3.fissile-linux/fissile-*.tgz --directory "/usr/local/bin/"
 
-VERSION=$(cat release/version)
-
-# Prepare stemcell
-STEMCELL_NAME="${STEMCELL_REPOSITORY}:$(cat s3.stemcell-version/*-version)"
-docker pull "${STEMCELL_NAME}"
+# Pull the stemcell image.
+stemcell_version="$(cat s3.stemcell-version/"${STEMCELL_VERSIONED_FILE##*/}")"
+stemcell_image="${STEMCELL_REPOSITORY}:${stemcell_version}"
+docker pull "${stemcell_image}"
 
 if [ ! -e release/sha1 ]; then
   # Calculate sha1sum if the resource is a file from s3. bosh.io resources provide the checksum automatically
@@ -27,8 +26,6 @@ else
   SHA1=$(cat release/sha1)
 fi
 
-s3.fissile-linux/fissile build release-images --stemcell="${STEMCELL_NAME}" --name="${RELEASE_NAME}" --version="${VERSION}" --sha1="$SHA1" --url="$(cat release/url)"
-
-BUILT_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "$STEMCELL_REPOSITORY" | head -1)
-docker tag "${BUILT_IMAGE}" "${REGISTRY_NAMESPACE}/${BUILT_IMAGE}"
-docker push "${REGISTRY_NAMESPACE}/${BUILT_IMAGE}"
+# Build the releases.
+tasks_dir="$(dirname $0)"
+bash <(yq -r ".manifest_version as \$cf_version | .releases[] | \"source ${tasks_dir}/build_release.sh; build_release \\(\$cf_version|@sh) '${DOCKER_REGISTRY}' '${DOCKER_ORGANIZATION}' '${DOCKER_TEAM_USERNAME}' '${DOCKER_TEAM_PASSWORD_RW}' '${STEMCELL_OS}' '${stemcell_version}' '${stemcell_image}' \\(.name|@sh) \\(.url|@sh) \\(.version|@sh) \\(.sha1|@sh)\"" "${RELEASE_NAME}")
